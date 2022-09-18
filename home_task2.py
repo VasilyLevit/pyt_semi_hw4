18234092b86053c367a8255c7e26da7e8ad46dbe
Vasily Levit <vasily.levit@gmail.com>
2022-09-18 23:47:09 +0800

    add task in еру first approximation

--- home_task2.py
+++ home_task2.py
@@ -1,2 +1,15 @@
 # 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 # "20" -> [2, 2, 5]
+
+n = 20
+
+def find_div(n):
+    dividers = []
+
+    for i in range(2, n):
+        while n % i == 0:
+            n //= i
+            dividers.append(i)
+    return dividers
+
+print(find_div(n))
--- home_task5.py
+++ home_task5.py
@@ -1,2 +1,55 @@
 # 5. Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
+
+with open('file1.txt','w') as data_one:
+    data_one.write('5x^2+2x+9')
+with open('file2.txt','w') as data_two:
+    data_two.write('12x^2+6x+1')
+
+data = open('file1.txt','r')
+for line in data:
+    lst_one = line.split('+')
+data.close
+
+data = open('file2.txt','r')
+for line in data:
+    lst_two = line.split('+')
+data.close
+
+print(lst_one)
+print(lst_two)
+
+lst_out1 = []
+for term in lst_one:
+    if 'x^' in term:
+        term_spl = term.split('x^')
+        lst_out1.append(term_spl[0])
+    elif 'x' in term:
+        term_spl = term.split('x')
+        lst_out1.append(term_spl[0])
+    else:
+        lst_out1.append(term)
+
+lst_out2 = []
+data = open('file12.txt', 'a')
+for term in lst_two:
+    i = 0
+    if 'x^' in term:
+        term_spl = term.split('x^')
+        lst_out2.append(term_spl[0])
+        data.write(str(int(lst_out1[i]) + int(term_spl[0])))
+        data.write('x^2+')
+
+    elif 'x' in term:
+        term_spl = term.split('x')
+        lst_out2.append(term_spl[0])
+        data.write(str(int(lst_out1[i+1]) + int(term_spl[0])))
+        data.write('x+')
+
+    else:
+        lst_out2.append(term)
+        data.write(str(int(lst_out1[i+2]) + int(term)))
+    i += 1
+
+data.close
+
