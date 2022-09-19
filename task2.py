03b7b538c6d6e0310666013e1671d3ea171e0f68
Vasily Levit <vasily.levit@gmail.com>
2022-09-19 21:33:31 +0800

    modifed task2

--- task2.py
+++ task2.py
@@ -3,23 +3,16 @@
 # *2) с помощью дополнительных библиотек Python
 # a * (x**2) + b * x + c = 0
 
-def xx(a, b, c):
+def root(a, b, c):
     d = (b * b) - (4 * a * c)
-    if d > 0:
-        d = d ** 0.5
-        x1 = (-b + d) / 2 * a
-        x2 = (-b - d) / 2 * a
+    if d >= 0:
+        x1 = (-b + d ** 0.5) / 2 * a
+        x2 = (-b - d ** 0.5) / 2 * a
         return x1, x2
-    if d == 0:
-        d = d ** 0.5
-        x = -b / 2 * a
-        return x
-    else:
-        print ('Корней нет')
-    # return None  # вместо else
+    return None
 
 a = 1
-b = -12
-c = 9
+b = 17
+c = -18
 
-print(xx(a, b, c))
+print(root(a, b, c))
