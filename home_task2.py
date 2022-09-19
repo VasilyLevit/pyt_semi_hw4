e96509b68dcb48de3244fd7ebdd3b1a41110b425
Vasily Levit <vasily.levit@gmail.com>
2022-09-19 13:35:37 +0800

    correction function

--- home_task2.py
+++ home_task2.py
@@ -1,15 +1,13 @@
 # 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 # "20" -> [2, 2, 5]
 
-n = 20
-
-def find_div(n):
-    dividers = []
-
+def find_prime(n):
+    prime_num = []
     for i in range(2, n):
         while n % i == 0:
             n //= i
-            dividers.append(i)
-    return dividers
+            prime_num.append(i)
+    return prime_num
 
-print(find_div(n))
+n = 52
+print(find_prime(n))
