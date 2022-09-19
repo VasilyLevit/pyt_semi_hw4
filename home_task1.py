f691f5b55037f28eaf809504c04b0996527282ad
Vasily Levit <vasily.levit@gmail.com>
2022-09-19 13:57:39 +0800

    task1 correction function

--- home_task1.py
+++ home_task1.py
@@ -1,8 +1,11 @@
 # 1. Вычислить число c заданной точностью d
 # Пример:при $d = 0.001, π = 3.141
 
-import math
-pi = str(math.pi)
-d = str(0.0001)
-l = len(d)
-print(pi[0:l])
+def round(n, d):
+    l = len(d)
+    return n[0:l]
+
+num = str(2 ** 0.5)
+accur = str(0.0001)
+print(num)
+print(round(num, accur))
