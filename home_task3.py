a5b0523dbb5fa51b283f11fb31209bdf15f21d3b
Vasily Levit <vasily.levit@gmail.com>
2022-09-19 14:11:25 +0800

    task3 add function

--- home_task3.py
+++ home_task3.py
@@ -15,9 +15,12 @@
 # print(list_res)
 
 # 2 вариант (с использованием функции count)**********
+def unique_num(list_in):
+        list_out = []
+        for i in list_in:
+                if (list_in.count(i)) == 1:
+                        list_out.append(i)
+        return list_out
+
 list = [1, 1, 2, 3, 4, 5, 5]
-result = []
-for i in list:
-     if (list.count(i)) == 1:
-        result.append(i)
-print(result)
+print(unique_num(list))
